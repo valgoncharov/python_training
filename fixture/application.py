9d36781a1441a8142f20bac0e26dd8f8f9f227a7
goncharov.vv <goncharov.vv@esphere.ru>
2024-08-01 20:31:11 +0300

    Добавление фикстуры

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 class Application:
 
 
@@ -7,6 +8,7 @@ class Application:
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
 
     def submit_the_form(self):
@@ -14,27 +16,6 @@ class Application:
         wd = self.wd
         wd.find_element_by_id("submit").click()
 
-    def set_address(self, location):
-        # set address
-        wd = self.wd
-        wd.find_element_by_id("currentAddress").send_keys(location.address)
-        wd.find_element_by_id("permanentAddress").click()
-        wd.find_element_by_id("permanentAddress").clear()
-        wd.find_element_by_id("permanentAddress").send_keys(location.address_str)
-
-    def set_email(self, email):
-        # set email
-        wd = self.wd
-        wd.find_element_by_id("userEmail").send_keys(email)
-        wd.find_element_by_id("currentAddress").click()
-        wd.find_element_by_id("currentAddress").clear()
-
-    def set_username(self, username):
-        # set username
-        wd = self.wd
-        wd.find_element_by_id("userName").send_keys(username)
-        wd.find_element_by_id("userEmail").click()
-        wd.find_element_by_id("userEmail").clear()
 
     def choose_item(self):
         # choose item
--- test/test_form.py
+++ test/test_form.py
@@ -16,9 +16,9 @@ def test_form(app):
     app.session.choose_form_page()
     app.session.go_to_text_box_page(text_box_url="https://demoqa.com/text-box")
     app.choose_item()
-    app.set_username(username="Ujy")
-    app.set_email(email="val@gmail.com")
-    app.set_address(Location(address="fsaa", address_str="acsd"))
+    app.group.set_username(username="Ujy")
+    app.group.set_email(email="val@gmail.com")
+    app.group.set_address(Location(address="fsaa", address_str="acsd"))
     app.submit_the_form()
 
 
@@ -27,7 +27,7 @@ def test_form_next(app):
     app.session.choose_form_page()
     app.session.go_to_text_box_page(text_box_url="")
     app.choose_item()
-    app.set_username(username="")
-    app.set_email(email="")
-    app.set_address(Location(address="", address_str=""))
+    app.group.set_username(username="")
+    app.group.set_email(email="")
+    app.group.set_address(Location(address="", address_str=""))
     app.submit_the_form()
