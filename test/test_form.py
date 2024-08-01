db6e51f943887da0ce07836039af44695e5e3fde
goncharov.vv <goncharov.vv@esphere.ru>
2024-08-01 21:37:58 +0300

    Добавление файла conftest

--- test/test_form.py
+++ test/test_form.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.location import Location
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
     
 def test_form(app):
